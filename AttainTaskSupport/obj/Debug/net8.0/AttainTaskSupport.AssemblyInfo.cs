ff79a778d2316f5645d5f0c065c494231b2a0879
ClintonOgembo <clintonogembo70@gmail.com>
2025-08-06 16:53:15 +0300

    btns

--- AttainTaskSupport/obj/Debug/net8.0/AttainTaskSupport.AssemblyInfo.cs
+++ AttainTaskSupport/obj/Debug/net8.0/AttainTaskSupport.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AttainTaskSupport")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98442c85e17f9ea3e2f8bf98dd0ccec3a6831c27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e06e99f5463be6acc9f8099a4f42fd344fdd006")]
 [assembly: System.Reflection.AssemblyProductAttribute("AttainTaskSupport")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AttainTaskSupport")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
